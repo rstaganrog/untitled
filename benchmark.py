43af93750328078db5421c9a8de42ce1cce1e7e7
rstaganrog <rstaganrog@mail.ru>
2019-06-25 14:23:34 +0300

    1

---
+++ benchmark.py
@@ -0,0 +1,21 @@
+def benchmark(func):
+    import time
+
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        print('[*] Время выполнения: {} секунд.'.format(end - start))
+
+    return wrapper
+
+
+@benchmark
+def cnt():
+    count_ = 0
+    for i in range(100*1000*1000):
+        count_ += 1
+    return count_
+
+
+cnt()
